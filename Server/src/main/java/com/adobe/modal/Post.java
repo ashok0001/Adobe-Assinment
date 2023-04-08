fa2443da3820c7eb81e3a92530ff5e7e14bc2456
Ashok Zarmariya <ashokzarmariya47@gmail.com>
2023-04-08 15:44:16 +0530

    create post entity

--- Server/src/main/java/com/adobe/modal/Post.java
+++ Server/src/main/java/com/adobe/modal/Post.java
@@ -1,5 +1,103 @@
 package com.adobe.modal;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.adobe.dto.UserDto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Post {
 
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	@ManyToOne
+	private User user;
+	
+	private String content;
+	
+	@Column(name="created_at")
+	private LocalDateTime createdAt;
+	
+	@Column(name="updated_at")
+	private LocalDateTime updatedAt;
+	
+	@Column(name="like_by_users")
+	@ManyToMany
+	private List<User> likedUser;
+
+	public Post(Integer id, User user, String content, LocalDateTime createdAt, LocalDateTime updatedAt,
+			List<User> likedUser) {
+		super();
+		this.id = id;
+		this.user = user;
+		this.content = content;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+		this.likedUser = likedUser;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public List<User> getLikedUser() {
+		return likedUser;
+	}
+
+	public void setLikedUser(List<User> likedUser) {
+		this.likedUser = likedUser;
+	}
+
+	
+	
+	
 }
